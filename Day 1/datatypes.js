b561f056155c51b14534a31286e0b892466996a6
ChiobiJason <chiobichisom@gmail.com>
2022-07-22 08:41:17 +0000

    Finished Day 1

---
+++ Day 1/datatypes.js
@@ -0,0 +1,9 @@
+console.log(typeof "Jason"); //sting
+
+console.log(typeof true); //boolean
+
+console.log(typeof 22); //number
+
+console.log(typeof null); //null
+
+console.log(typeof undefined); //undefined
---
+++ Day 1/variable.js
@@ -0,0 +1,26 @@
+let name = "Jason" //string
+
+let isCoding = true; //boolean
+
+let day = 22; //number
+
+let number = null; //null
+
+let gtithub; //undefined
+
+let firstName = "Chisom";
+
+let lastName = "Chiobi";
+
+let maritalStatus = "Single";
+
+let country = "Nigeria";
+
+let age = 16;
+
+let myAge = age;
+
+let yourAge = 25;
+
+console.log(`I am ${myAge} years old`);
+console.log(`You are ${yourAge} years old`);
